329445cf3458e0f864ba71224fe7e2ba63298c06
ChandlerBing46 <izhar1022@gmail.com>
2020-06-06 20:12:58 +0200

    Finally made the functions and stuff

--- C 3 Assignment/secondIdead.cpp
+++ C 3 Assignment/secondIdead.cpp
@@ -7,42 +7,79 @@ const int Kmax = 50;
 const int Kmin = 20;
 const int Tmin = 0;
 
+//Helper Functions Declarations and definitions
 
-
-
-void Solution(int N, int M, int K, int indCnt, int MaxCount, int index, int Data[])
+void inputs (int &N, int &M, int &K)
 {
-	for (int i = 1; i <= N; i++)
-    {
-		cout << i << ". Settlement: " << endl; 
-        for (int j = 1; j <= M; j++)
-        {
-			cout << "   " << j << ". Day's Temperature : ";
-			while(!(cin >> Data[j]) || Data[j] < Tmin || Data[j] > Kmax)
-			{
-				cout << "   Error! give me a number between "<< Tmin  << " and " << Kmax << " : ";
-				cin.clear();
-				cin.ignore(1000,'\n');
-			}
-		}
-        for (int k = 1; k<=M; k++)
-        {
-            indCnt = 0;
-            int m = k;
-            while (Data[m] > K)
-            {
-                indCnt++;
-                m++;
-            }
-            if (MaxCount < indCnt)
-            {
-                MaxCount = indCnt;
-                index = i;
-            }
-
-        }
+	clog << "N: ";
+   	while(!(cin >> N) || N > nMax || N < nMin)
+	{
+		cout << "Error! give me a number between " << nMin << " and " << nMax << " : ";
+		cin.clear();
+		cin.ignore(10000,'\n');
+	}
+	clog << "M: ";
+	while(!(cin >> M ) || M > nMax || M < nMin)
+	{
+		clog << "Error! give me a number between " << nMin << " and " << nMax << " : ";
+		cin.clear();
+		cin.ignore(10000,'\n');
+	}
+	clog << "K: ";
+    while(!(cin >> K ) || K > Kmax || K < Kmin)
+	{
+		clog << "Error! give me a number between " << Kmin << " and " << Kmax << " : ";
+		cin.clear();
+		cin.ignore(10000,'\n');
 	}
 }
+
+
+void Solution(int &N, int &M, int &K, int &indCnt, int &MaxCount, int &index, int Data[])
+{
+	for (int i = 1; i <= N; i++)
+  	{
+			clog << i << ". Settlement: " << endl; 
+       		for (int j = 1; j <= M; j++)
+       		{
+				clog << "   " << j << ". Day's Temperature : ";
+				while(!(cin >> Data[j]) || Data[j] < Tmin || Data[j] > Kmax)
+				{
+					clog << "   Error! give me a number between "<< Tmin  << " and " << Kmax << " : ";
+					cin.clear();
+					cin.ignore(1000,'\n');
+				}
+			}
+    	 	for (int k = 1; k<=M; k++)
+    	 	{
+       	  		indCnt = 0;
+        		int m = k;
+         		while (Data[m] > K)
+         		{
+              		indCnt++;
+          	    	m++;
+         		}
+       			if (MaxCount < indCnt)
+         		{
+          			MaxCount = indCnt;
+             	 	index = i;
+         		}
+
+     		}
+ 	}
+}
+
+
+outputs(int &index) 
+{
+	clog << endl;
+   	clog << "OutPuts" << endl;
+    cout << "The index of the settlement which has the longest period: " << index << endl;
+}
+
+
+
+
 int main()
 {
 	//Declarations
@@ -52,42 +89,24 @@ int main()
     int index = -1;
 	
 	//Inputs
-	cout << "N: ";
-    while(!(cin >> N) || N > nMax || N < nMin)
-	{
-		cout << "Error! give me a number between " << nMin << " and " << nMax << " : ";
-		cin.clear();
-		cin.ignore(10000,'\n');
-	}
-	cout << "M: ";
-	while(!(cin >> M ) || M > nMax || M < nMin)
-	{
-		cout << "Error! give me a number between " << nMin << " and " << nMax << " : ";
-		cin.clear();
-		cin.ignore(10000,'\n');
-	}
-	cout << "K: ";
-    while(!(cin >> K ) || K > Kmax || K < Kmin)
-	{
-		cout << "Error! give me a number between " << Kmin << " and " << Kmax << " : ";
-		cin.clear();
-		cin.ignore(10000,'\n');
-	}
+	inputs(N, M, K);
+	clog << N << " " << M << " " << K << endl; 
     int n = ((N+1) * (M+1));
-    int Data[n];  //declared this late becuse we need M*N times elements in the array, and for that we need M and N given in from the user side
+	int Data[n];  
 	
+	//algorithm implemention
 	Solution(N, M, K, indCnt, MaxCount, index, &Data[n]);
 	
-	
-	///Output 
-	cerr << endl;
-	cerr << endl;
-    cerr << "OutPuts" << endl;
-	cout << "The index of the settlement which has the longest period: " << index << endl;
-    
+	//Outputs 
+	outputs(index);
     return 0;
 
 }
+
+
+
+
+
 
 
 
